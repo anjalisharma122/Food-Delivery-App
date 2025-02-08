fb9348c175036a47a54ea687ebad9bba7aaffe6b
Anjali <anjalisharma288863@gmail.com>
2025-02-08 23:17:36 +0530

    using API instead of hardcoded mock data

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,31 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
-import resList from "../utils/mockData";
+import { useState, useEffect} from "react";
+import  Shimmer from "./Shimmer";
+
 
 const Body = ()=>{
  
-  const [listOfRestaurants ,setlistOfRestaurants] = useState(resList);
+  const [listOfRestaurants ,setlistOfRestaurants] = useState([]);
 
- 
+  useEffect(()=>{
+    fetchData(); 
+  },[]);
+  
+  const fetchData =async() =>{
+      const data = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      );
+  
+
+      const json = await data.json();
+      console.log(json);
+      setlistOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    };
+
+      if(listOfRestaurants.length ===0){
+        return <Shimmer/>;
+      }
+      
     return( 
         <div className="body">
             <div className="filter">
