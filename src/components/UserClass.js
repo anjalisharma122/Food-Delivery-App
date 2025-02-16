dd8dbef880956fbda7b27e453291da0b178f8fdb
Anjali <anjalisharma288863@gmail.com>
2025-02-16 23:44:50 +0530

    updated  code

---
+++ src/components/UserClass.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class UserClass extends React.Component {
+    constuctor(props){
+        super(props);
+
+        console.log(props);
+    }
+    render(){
+        return(
+            <div className="user-card">
+            <h2>Name :{this.props.name}</h2>
+            <h3>Location : Pune</h3>
+            <h4>Contact : @anjalisharma18</h4>
+        </div>
+        )
+    }
+}
+
+export default UserClass;
