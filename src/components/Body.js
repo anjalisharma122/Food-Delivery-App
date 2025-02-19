721a20aa11af9177fabd465d3d855eeb537c5f38
Anjali <anjalisharma288863@gmail.com>
2025-02-19 13:49:35 +0530

    updated code

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect} from "react";
 import  Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus" ;
 
 
 const Body = ()=>{
@@ -29,6 +30,15 @@ const Body = ()=>{
    
     setFilteredRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     };
+    const onlineStatus =useOnlineStatus() ;
+
+    if(onlineStatus === false) 
+      return (
+        <h1>
+          Looks like you're offline !! Please check your internet ;
+        </h1>
+      )
+     
 
      
      return listOfRestaurants.length ===0 ?(
