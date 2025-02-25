ff95c993c4b7f5b997dcadf14be57a0e5adc5b6d
Anjali <anjalisharma288863@gmail.com>
2025-02-25 20:11:26 +0530

    updated code

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,7 +1,14 @@
-
+import {useDispatch} from "react-redux";
+import { addItem } from "../utils/cartSlice";
 import {CDN_URL} from "../utils/constants";
 const ItemList =({items})=>{
 
+    const dispatch =useDispatch();
+
+    const handleAddItem=()=>{
+        //dispatch an action
+        dispatch(addItem("pizza"));
+    };
     return (
         <div>
             {items.map((item)=>(
@@ -21,7 +28,10 @@ const ItemList =({items})=>{
                     </div>
                     <div className="w-3/12 p-4">
                         <div className="absolute ">
-                        <button className="p-2 bg-black text-white shadow-lg mx-16 rounded-lg ">Add +</button>
+                        <button className="p-2 bg-black text-white shadow-lg mx-16 rounded-lg "
+                        onClick ={handleAddItem}>
+                            Add +
+                        </button>
                         </div>
                         <img src={CDN_URL + item.card.info.imageId} className="w-full" />
                     </div>
@@ -38,4 +48,4 @@ const ItemList =({items})=>{
     )
 }
 
-export default ItemList
+export default ItemList;
