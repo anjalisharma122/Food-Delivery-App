07c4c6c8f46c4e61d48d3982c33633b922950678
Anjali <anjalisharma288863@gmail.com>
2025-02-27 01:42:38 +0530

    updating code

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,12 +1,23 @@
 import { useSelector } from "react-redux";
 import ItemList from "./ItemList";
+import { clearCart } from "../utils/cartSlice";
+import { useDispatch } from "react-redux";
 const Cart = () =>{
     const cartItems = useSelector((store)=>store.cart.items); 
+
+    const dispatch=useDispatch();
+    const handleClearCart =()=>{
+        dispatch(clearCart());
+    } 
  
 
     return <div className="text-center m-4 p-4">
         <h1 className="text-2xl font-bold">Cart</h1>
-        <div>
+        <div className="w-6/12 m-auto">
+        <button className="p-2 m-2 bg-black text-white rounded-lg cursor-pointer" onClick={handleClearCart}>
+        Clear Cart
+        
+        </button>
             <ItemList items={cartItems} />
         </div>
     </div>
