5c7b07b590f2900db249db089b88d8bd7616552c
Anjali <anjalisharma288863@gmail.com>
2025-02-28 02:46:27 +0530

    added test cases

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -2,12 +2,35 @@ import Contact from "../Contact";
 import { render ,screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
-test("Should load contact us component" ,()=>{
+test("Should load heading inside contact component" ,()=>{
     render(<Contact />);
 
-
+    //Assertion
     const heading=screen.getByRole("heading");
-
-
     expect(heading).toBeInTheDocument();
 });
+
+test("Should load button inside contact component" ,()=>{
+    render(<Contact />);
+    
+    //Assertion
+    const button=screen.getByText("Submit");
+    expect(button).toBeInTheDocument();
+});
+
+test("Should load input name inside contact component" ,()=>{
+    render(<Contact />);
+    
+    //Assertion
+    const input=screen.getByPlaceholderText("name");
+    expect(input).toBeInTheDocument();
+});
+
+test("Should load 2 input boxes on the component component " ,()=>{
+    render(<Contact/>)
+
+    const inputBoxes = screen.getAllByRole("textbox");
+
+    expect(inputBoxes.length).toBe(2);
+})
+
