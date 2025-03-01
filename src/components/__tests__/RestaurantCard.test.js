08498b78fd0fee8e526d3768d1936bfcaaf5a533
Anjali <anjalisharma288863@gmail.com>
2025-03-01 17:23:51 +0530

    updated tests

---
+++ src/components/__tests__/RestaurantCard.test.js
@@ -0,0 +1,14 @@
+import {render , screen } from "@testing-library/react";
+import RestaurantCard  from "../../components/RestaurantCard";
+console.log("RestaurantCard:", RestaurantCard);
+import MOCK_DATA from "../mocks/resCardMock.json";
+import "@testing-library/jest-dom";
+
+test("Should render restaurantcard component with props data" ,()=>{
+    console.log("Mock Data" , MOCK_DATA);
+    render(<RestaurantCard resData={MOCK_DATA} />);
+
+    const name=screen.getByText("Pizza Hut");
+
+    expect(name).toBeInTheDocument();
+})
---
+++ src/components/__tests__/tempCodeRunnerFile.js
@@ -0,0 +1,13 @@
+import {render , screen } from "@testing-library/react";
+import RestaurantCard  from "../../components/RestaurantCard";
+
+import MOCK_DATA from "../mocks/resCardMock.json";
+import "@testing-library/jest-dom";
+
+test("Should render restaurantcard component with props data" ,()=>{
+    render(<RestaurantCard resData={MOCK_DATA} />);
+
+    const name=screen.getByText("Pizza Hut");
+
+    expect(name).toBeInTheDocument();
+})
